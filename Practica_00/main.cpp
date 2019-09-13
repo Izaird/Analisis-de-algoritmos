2ce7ccfdd492df355ccbdf5ccbe47221eb53e65d
izaird <iza_alex3@hotmail.com>
2019-09-13 03:29:53 -0500

    Lectura de estados, alfabeto, estado inicial y estados de aceptacion

--- Practica_00/main.cpp
+++ Practica_00/main.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
 
-class Estado{
-    private:
-        int numero;
-        int inicial;
-        int final;
-}
+vector<string> str_to_vect(string str);
 
 int main(){
+  fstream file;
   string line;
-  ifstream myfile ("archivo.txt");
-  if (myfile.is_open())
-  {
-    while ( getline (myfile,line) )
-    {
-      cout << line << '\n';
+  vector<string> states, alphabet, start, accept;
+  file.open("archivo2.txt");
+  file >> line;  
+  states = str_to_vect(line);
+  file >> line;  
+  alphabet = str_to_vect(line);
+  file >> line;  
+  start.push_back(line);
+  file >> line;  
+  accept = str_to_vect(line);
+    for (std::size_t i = 0; i < states.size(); i++)
+        std::cout << states[i] << std::endl;
+}
+
+
+
+vector<string> str_to_vect(string str){
+  vector<string> vect;
+  string aux;
+  for(int i=0; i < str.length(); i++){
+    if(str[i]==44){
+      vect.push_back(aux);
+      aux.clear();
     }
-    myfile.close();
+    else
+      aux += str[i];
   }
+  vect.push_back(aux);
+  aux.clear();
 
-  else cout << "Unable to open file"; 
-
-  return 0;
+  return vect;
 }
