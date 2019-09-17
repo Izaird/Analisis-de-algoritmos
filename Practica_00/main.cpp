3027743ceb2dfe102f74f37773dd520d7ad764b0
izaird <iza_alex3@hotmail.com>
2019-09-16 23:55:35 -0500

    Lectura y guardado de transiciones en array

--- Practica_00/main.cpp
+++ Practica_00/main.cpp
@@ -2,36 +2,52 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
+struct transition{
+  std::string actual_state,event,next_state;
+};
 
+std::ostream& operator<<(std::ostream& stream, const transition& transition){
+  stream << transition.actual_state << ","<< transition.event << "," << transition.next_state;
+  return stream;
+}
 
-
-
-
-std::vector<std::string> str_to_vect(std::string str);
+//DECLARATIONS
+std::vector<std::string> StringToVector(std::string str);
+std::vector<std::string> GetLines(std::string file);//Save all the lines of the document in an arrangement to have better handling on these
+std::vector<transition> ReadTransitions(const std::vector<std::string> &lines);
 
 int main(){
-  std::fstream file;
-  std::string line;
-  std::vector<std::string> states, alphabet, start, accept;
-  file.open("archivo2.txt");
-  file >> line;  
-  states = str_to_vect(line);
-  file >> line;  
-  alphabet = str_to_vect(line);
-  file >> line;  
-  start.push_back(line);
-  file >> line;  
-  accept = str_to_vect(line);
-    for (std::size_t i = 0; i < states.size(); i++)
-        std::cout << states[i] << std::endl;
-
+  std::vector<std::string> lines, states, alphabet, start, accept;
+  std::vector<transition> transitions;
+  lines = GetLines("input");
+  states = StringToVector(lines[0]);
+  alphabet = StringToVector(lines[1]);
+  start = StringToVector(lines[2]);
+  accept = StringToVector(lines[3]);
+  transitions = ReadTransitions(lines);
+  for (transition& T : transitions)
+  {
+    std::cout << T << std::endl;
+  }
   
+
 }
 
 
+//DEFINITIONS 
+std::vector<std::string> GetLines(std::string name_file){
+  std::vector<std::string> Lines;
+  std::string line;
+  std::fstream file;
+  file.open(name_file);
+  while (getline(file, line)){
+    Lines.push_back(line);
+  }
+  return Lines;
+}
 
-
-std::vector<std::string> str_to_vect(std::string str){
+std::vector<std::string> StringToVector(std::string str){
   std::vector<std::string> vect;
   std::string aux;
   for(int i=0; i < str.length(); i++){
@@ -47,3 +63,23 @@ std::vector<std::string> str_to_vect(std::string str){
 
   return vect;
 }
+
+std::vector<transition> ReadTransitions(const std::vector<std::string> &lines){
+  std::vector<transition> transitions;//array to save the transitions 
+  std::string A,B,C,aux;
+  std::stringstream ss;
+
+  for(int i= 4; i < lines.size(); i++){
+    aux = lines[i];
+    std::stringstream ss(aux);
+    getline(ss,aux, ',');
+    A=aux;
+    getline(ss,aux, ',');
+    B=aux;
+    getline(ss,aux, ',');
+    C=aux;  
+    transitions.push_back({A, B, C});
+  }
+  return transitions;
+}
+
