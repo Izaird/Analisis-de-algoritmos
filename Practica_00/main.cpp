04b0d23b40bb03f4cd9fea41071b231d9c6b8f97
izaird <iza_alex3@hotmail.com>
2019-09-19 11:23:02 -0500

    fillStates

--- Practica_00/main.cpp
+++ Practica_00/main.cpp
@@ -4,26 +4,27 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <map>
+
 
 struct transition{
   std::string event;  
   int next_state;
 
-  void setTransition(std::string a, int b){
+  transition(std::string a, int b){
   event = a;
   next_state = b;
   }
 };
-
-
-
 
 std::ostream& operator<<(std::ostream& stream, const transition& transition){
   stream << transition.event << "," << transition.next_state;
   return stream;
 }
 
-//CLASSES
+/*
+CLASSES
+*/
 class State{
 private:
   bool initial, accepted;
@@ -35,27 +36,58 @@ public:
   int getNumber();
   bool getInitial();
   bool getAccepted();
+  std::vector<std::string> getEventsOfTransitions();
+  int numberOfTransitions();
 };
 
+/*
+METHODS
+*/
 void State::setState(int a, bool b, bool c){
   number = a;
   initial = b;
   accepted = c;
 }
 
-  void State::setTransition(transition transition){transitions.push_back(transition);}
-  int State::getNumber(){return number;}
-  bool State::getInitial(){return initial;}
-  bool State::getAccepted(){return accepted;}
+int State::numberOfTransitions(){
+    return transitions.size();
+}
 
+void State::setTransition(transition transition){
+    transitions.push_back(transition);
+}
 
+int State::getNumber(){
+    return number;
+}
 
-//DECLARATIONS
+bool State::getInitial(){
+    return initial;
+}
+
+bool State::getAccepted(){
+    return accepted;
+}
+
+std::vector<std::string> State::getEventsOfTransitions(){
+    std::vector<std::string> events;
+    for(int i = 0; i < transitions.size(); i++){
+      events.push_back(transitions[i].event);
+    }
+    return events;
+}
+/*
+DECLARETIONS
+*/
+void uniqueElements(std::vector<std::string>& vec);
 transition stringToTransition(int i, const std::vector<std::string>& lines);
 int getFirstElement(std::string line);
+void fillStates(std::vector<State> &obj_states, const std::vector<std::string>& alphabet);
 std::vector<std::string> StrToVectStr(std::string str);
 std::vector<int> StrToVectInt(std::string str);
 std::vector<std::string> GetLines(std::string file);//Save all the lines of the document in an arrangement to have better handling on these
+std::vector<std::string> subVectorfromVector(std::vector<std::string> &vector1, const std::vector<std::string> &vector2);
+
 
 int main(){
   std::vector<int> states, initial_state, acceptance_states;
@@ -63,7 +95,7 @@ int main(){
   std::vector<transition> all_transitions;
   std::vector<State> obj_states;
   std::vector<transition> aux;
-  lines = GetLines("input");
+  lines = GetLines("inputs/input");
   states = StrToVectInt(lines[0]);
   states.push_back(-1);
   alphabet = StrToVectStr(lines[1]);
@@ -73,19 +105,15 @@ int main(){
 
   for(int i=0; i<states.size(); i++){
     bool initial, final;
-    if(std::find(initial_state.begin(), initial_state.end(), states[i]) != initial_state.end()) {
+    if(std::find(initial_state.begin(), initial_state.end(), states[i]) != initial_state.end())
       initial = true;
-    } 
-    else{
+    else
       initial = false;   
-    }
 
-    if(std::find(acceptance_states.begin(), acceptance_states.end(), states[i]) != acceptance_states.end()){
+    if(std::find(acceptance_states.begin(), acceptance_states.end(), states[i]) != acceptance_states.end())
       final = true;
-    } 
-    else{
+    else
       final = false;
-    }
   State state;
   state.setState(states[i],initial,final);
   obj_states.push_back(state);
@@ -102,27 +130,27 @@ int main(){
           found =false;
         }
         else
-        {
           j++;
-        }
-        
       }
-    
-    
   }
-
-
+  
+  fillStates(obj_states,alphabet);
   return 0;
 }
 
 
-//DEFINITIONS 
+
+
+/*
+DEFINITIONS
+*/ 
 std::vector<std::string> GetLines(std::string name_file){
   std::vector<std::string> Lines;
   std::string line;
   std::fstream file;
   file.open(name_file);
   while (getline(file, line)){
+    if(!line.empty())
     Lines.push_back(line);
   }
   return Lines;
@@ -186,14 +214,42 @@ transition stringToTransition(int i, const std::vector<std::string>& lines){
   std::string B,aux;
   aux = lines[i];
   int C;
-  transition transition_line;
+  
   std::stringstream ss(aux);
   getline(ss,aux, ',');
   getline(ss,aux, ',');
   B=aux;
   getline(ss,aux, ',');
-  C=std::stoi(aux);  
-  transition_line.setTransition(B,C);
+  C=std::stoi(aux); 
+  transition transition_line(B,C);
   return transition_line;
 }
 
+void fillStates(std::vector<State> &obj_states, const std::vector<std::string>& alphabet){
+  std::vector<std::string> events, missing_events;
+  for (int i = 0; i < obj_states.size(); i++){
+    events = obj_states[i].getEventsOfTransitions();
+    uniqueElements(events);
+    missing_events = subVectorfromVector(events,alphabet);
+    for (int j = 0; j < missing_events.size(); j++){
+      transition transition_1(missing_events[j],-1);
+      obj_states[i].setTransition(transition_1);
+    }
+    
+  }
+  
+}
+
+void uniqueElements(std::vector<std::string>& vec){
+vec.erase( unique( vec.begin(), vec.end() ), vec.end() );
+vec.erase(std::remove(vec.begin(), vec.end(), "E"), vec.end());
+}
+
+
+std::vector<std::string> subVectorfromVector(std::vector<std::string> &vector1, const std::vector<std::string> &vector2){
+  std::vector<std::string> vec = vector2;
+  for (int i = 0; i < vector1.size(); i++){
+   vec.erase(std::remove(vec.begin(), vec.end(), vector1[i]), vec.end()); 
+  }
+  return vec;
+}
