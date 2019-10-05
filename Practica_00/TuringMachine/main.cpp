55b9e31d98a6ef7e67fc4af23358d2e5fef0ea76
izaird <iza_alex3@hotmail.com>
2019-10-05 16:10:40 -0500

    recursive algorithm

--- Practica_00/TuringMachine/main.cpp
+++ Practica_00/TuringMachine/main.cpp
@@ -1,5 +1,7 @@
 #include "turing.h"
 #include "color.h"
+void travel_r(int actual_state, int next_state, char tape_actual_char, Tape &tape, std::vector<State> &states);
+
 
 int main(){
     std::vector<State> states;
@@ -13,21 +15,44 @@ int main(){
 
     actual_state = getInitialState(states);
 
-    do{
+    // do{
+    //     tape_actual_char = tape.read();
+    //     next_state = states[actual_state].getNextState(tape_actual_char);
+    //     tape.update(states[actual_state].getNewEvent(tape_actual_char));
+    //     tape.move(states[actual_state].getDirection(tape_actual_char));
+    //     actual_state = next_state;
+    //     if(states[actual_state].getAcceptance()){
+    //         std::cout << "string accepted" << std::endl;
+    //         run = false;
+    //     }
+    //     else if(actual_state == -1){
+    //         std::cout << "string rejected" << std::endl;
+    //         run = false;
+    //     }
+    // }while(run);
+
+    travel_r(actual_state,next_state,tape_actual_char,tape,states);
+
+    std::cout << "end" << std::endl;
+
+
+    return 0;
+}
+
+
+    void travel_r(int actual_state, int next_state, char tape_actual_char, Tape &tape, std::vector<State> &states){
+        if(states[actual_state].getAcceptance()){
+            std::cout << "string accepted" << std::endl;
+        }
+        else if(actual_state == -1){
+            std::cout << "string rejected" << std::endl;
+        }
+        else{
         tape_actual_char = tape.read();
         next_state = states[actual_state].getNextState(tape_actual_char);
         tape.update(states[actual_state].getNewEvent(tape_actual_char));
         tape.move(states[actual_state].getDirection(tape_actual_char));
         actual_state = next_state;
-        if(states[actual_state].getAcceptance()){
-            std::cout << "string accepted" << std::endl;
-            run = false;
+        travel_r(actual_state,next_state,tape_actual_char,tape,states);
         }
-        else if(actual_state == -1){
-            std::cout << "string rejected" << std::endl;
-            run = false;
-        }
-    }while(run);
-
-    return 0;
-}
+    }
