51da13a6eb8207c368adfec55ddd043bb121f41a
izaird <iza_alex3@hotmail.com>
2019-10-10 06:49:44 -0500

    funcion recursiva travel

--- Practica_00/TuringMachine/main.cpp
+++ Practica_00/TuringMachine/main.cpp
@@ -3,7 +3,6 @@
 
 int main(){
     std::vector<State> states;
-    std::string path;
     // std::vector<char> test= {'_','a','a','a','b','b','b','_'};
     char event, tape_actual_char;
     int actual_state, next_state;
@@ -14,6 +13,16 @@ int main(){
 
     actual_state = getInitialState(states);
 
+    // travel_r(actual_state,next_state,tape_actual_char,tape,states);
+    travel(actual_state,next_state,tape_actual_char,tape,states);
+
+
+    return 0;
+}
+
+
+void travel(int actual_state, int next_state, char tape_actual_char, Tape &tape, std::vector<State> &states){
+    std::string path;
     do{
         tape_actual_char = tape.read();
         next_state = states[actual_state].getNextState(tape_actual_char);
@@ -32,8 +41,24 @@ int main(){
             break;
         }
         else
-            path += "\u2192";
+        path += "\u2192";
     }while(true);
+}
 
-    return 0;
+
+void travel_r(int actual_state, int next_state, char tape_actual_char, Tape &tape, std::vector<State> &states){
+    if(states[actual_state].getAcceptance()){
+        std::cout << "string accepted" << std::endl;
+    }
+    else if(actual_state == -1){
+        std::cout << "string rejected" << std::endl;
+    }
+    else{
+    tape_actual_char = tape.read();
+    next_state = states[actual_state].getNextState(tape_actual_char);
+    tape.update(states[actual_state].getNewEvent(tape_actual_char));
+    tape.move(states[actual_state].getDirection(tape_actual_char));
+    actual_state = next_state;
+    travel_r(actual_state,next_state,tape_actual_char,tape,states);
+    }
 }
